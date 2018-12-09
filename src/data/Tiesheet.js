a825993da36fbea507729f321ad5f066655b7f38
ananta <anantarajbastola@gmail.com>
2018-12-09 23:15:22 +0545

    Data Updated.

---
+++ src/data/Tiesheet.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    day: 'Day 1',
+    tiesheets: [
+      {
+        title: 'Cricket Tiesheet',
+        link: 'https://drive.google.com/uc?export=download&id=1Jk9sxGOTn5rRHH2OL0RqAkKlq1XDlCzC',
+        description: 'This is a Tiesheet for Cricket'
+      },
+      {
+        title: 'Tiesheet Name',
+        link: 'https://google.com',
+        description: 'Description of Tiesheet'
+      },
+      {
+        title: 'Tiesheet Name',
+        link: 'https://google.com',
+        description: 'Description of Tiesheet'
+      },
+      {
+        title: 'Tiesheet Name',
+        link: 'https://google.com',
+        description: 'Description of Tiesheet'
+      }
+    ]
+  },
+  {
+    day: 'Day 2',
+    tiesheets: [
+      {
+        title: 'Tiesheet Name',
+        link: 'https://google.com',
+        description: 'Description of Tiesheet'
+      },
+      {
+        title: 'Tiesheet Name',
+        link: 'https://google.com',
+        description: 'Description of Tiesheet'
+      },
+      {
+        title: 'Tiesheet Name',
+        link: 'https://google.com',
+        description: 'Description of Tiesheet'
+      }
+    ]
+  }
+]
---
+++ src/data/TimeTable.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    day: "DAY 1",
+    timetable: [
+      {
+        time: '6 : 30 AM',
+        description: 'OPENING CEREMONY'
+      },
+      {
+        time: '7 : 00 AM',
+        description: 'TUG OF WAR '
+      },
+      {
+        time: '8 : 00 AM - 10 : 00 AM',
+        description: 'BASKETBALL + TABLE T. + CHESS'
+      }
+    ]
+  },
+  {
+    day: "DAY 1",
+    timetable: [
+      {
+        time: '6 : 30 AM',
+        description: 'OPENING CEREMONY'
+      },
+      {
+        time: '7 : 00 AM',
+        description: 'TUG OF WAR '
+      },
+      {
+        time: '8 : 00 AM - 10 : 00 AM',
+        description: 'BASKETBALL + TABLE T. + CHESS'
+      }
+    ]
+  }
+]
