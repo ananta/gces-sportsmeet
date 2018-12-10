eab5506b7976b1863bd85a26f727a88af9db410f
ananta <anantarajbastola@gmail.com>
2018-12-10 06:02:41 +0545

    TImetable tiesheet updated

--- src/data/Tiesheet.js
+++ src/data/Tiesheet.js
@@ -3,45 +3,50 @@ export default [
     day: 'Day 1',
     tiesheets: [
       {
-        title: 'Cricket Tiesheet',
+        title: 'CRICKET',
         link: 'https://drive.google.com/uc?export=download&id=1Jk9sxGOTn5rRHH2OL0RqAkKlq1XDlCzC',
         description: 'This is a Tiesheet for Cricket'
       },
       {
-        title: 'Tiesheet Name',
+        title: 'FOOTBALL + VOLLEYBALL + BASKETBALL',
         link: 'https://google.com',
-        description: 'Description of Tiesheet'
+        description: 'Tiesheeets of FOOTBALL, VOLLEYBALL, BASKETBALL'
       },
       {
-        title: 'Tiesheet Name',
+        title: 'BADMINTON',
         link: 'https://google.com',
-        description: 'Description of Tiesheet'
+        description: 'Tiesheet for Badminton'
       },
       {
-        title: 'Tiesheet Name',
+        title: 'CHESS',
         link: 'https://google.com',
-        description: 'Description of Tiesheet'
+        description: 'Tiesheet for chess'
+      },
+      {
+        title: 'TABLE TENNIS',
+        link: 'https://google.com',
+        description: 'Tiesheet for chess'
       }
     ]
   },
-  {
-    day: 'Day 2',
-    tiesheets: [
-      {
-        title: 'Tiesheet Name',
-        link: 'https://google.com',
-        description: 'Description of Tiesheet'
-      },
-      {
-        title: 'Tiesheet Name',
-        link: 'https://google.com',
-        description: 'Description of Tiesheet'
-      },
-      {
-        title: 'Tiesheet Name',
-        link: 'https://google.com',
-        description: 'Description of Tiesheet'
-      }
-    ]
-  }
+  // {
+  //   day: 'Day 2',
+  //   tiesheets: [
+  //     {
+  //       title: 'Tiesheet Name',
+  //       link: 'https://google.com',
+  //       description: 'Description of Tiesheet'
+  //     },
+  //     {
+  //       title: 'Tiesheet Name',
+  //       link: 'https://google.com',
+  //       description: 'Description of Tiesheet'
+  //     },
+  //     {
+  //       title: 'Tiesheet Name',
+  //       link: 'https://google.com',
+  //       description: 'Description of Tiesheet'
+  //     }
+  //   ]
+  // }
 ]
--- src/data/TimeTable.js
+++ src/data/TimeTable.js
@@ -13,24 +13,36 @@ export default [
       {
         time: '8 : 00 AM - 10 : 00 AM',
         description: 'BASKETBALL + TABLE T. + CHESS'
+      },
+      {
+        time: '10 : 00 AM - 11 : 00 AM',
+        description: 'BREAK'
+      },
+      {
+        time: '11 : 00 AM - 1 : 00 PM',
+        description: 'VOLLEYBALL + BADMINTON'
+      },
+      {
+        time: '1 : 00 PM - 4 : 00 PM',
+        description: 'FOOTBALL'
       }
     ]
   },
-  {
-    day: "DAY 1",
-    timetable: [
-      {
-        time: '6 : 30 AM',
-        description: 'OPENING CEREMONY'
-      },
-      {
-        time: '7 : 00 AM',
-        description: 'TUG OF WAR '
-      },
-      {
-        time: '8 : 00 AM - 10 : 00 AM',
-        description: 'BASKETBALL + TABLE T. + CHESS'
-      }
-    ]
-  }
+  // {
+  //   day: "DAY 1",
+  //   timetable: [
+  //     {
+  //       time: '6 : 30 AM',
+  //       description: 'OPENING CEREMONY'
+  //     },
+  //     {
+  //       time: '7 : 00 AM',
+  //       description: 'TUG OF WAR '
+  //     },
+  //     {
+  //       time: '8 : 00 AM - 10 : 00 AM',
+  //       description: 'BASKETBALL + TABLE T. + CHESS'
+  //     }
+  //   ]
+  // }
 ]
