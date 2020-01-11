8cab9b0f0dd37e5f18c4db23f8de3980ee7d7570
Ananta Bastola <anantarajbastola@gmail.com>
2020-01-11 21:43:53 +0545

    Update landing.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
-import Slide from '../components/Slide';
-import Events from '../data/Events';
-import { EventsURL } from './';
+import "./index.css";
+import Slide from "../components/Slide";
+import Events from "../data/Events";
+import { EventsURL } from "./";
 class Landing extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       events: [],
       isBusy: true,
-      error: null,
+      error: null
     };
   }
 
@@ -23,70 +23,115 @@ class Landing extends React.Component {
     this.setState({ isBusy: true });
     try {
       const events = await fetch(dataURL);
-      const _events = await events.json()
+      const _events = await events.json();
       this.setState({ events: _events });
     } catch (exp) {
-      this.setState({ error: exp.message })
+      this.setState({ error: exp.message });
     }
     this.setState({ isBusy: false });
   }
 
   render() {
-    const {isBusy, error, events} = this.state;
-    if(!events && error) return (<h1>Data Unavailable</h1>)
+    const { isBusy, error, events } = this.state;
+    if (!events && error) return <h1>Data Unavailable</h1>;
     return (
       <section className="wrapper">
         <div className="container main-background">
           <div className="row">
-            <div className="col-md-6" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-              <div style={{ padding: '32px 0' }}>
+            <div
+              className="col-md-6"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center"
+              }}
+            >
+              <div style={{ padding: "32px 0" }}>
                 <div className="title">
-                    <small className="title--event">7<sup>th</sup></small>
-                    <h1 className="title--gces">GCES</h1>
-                    <h2 className="title--expo">SPORTS MEET</h2>
-                    <p className="title--year">2018</p>
-                    <blockquote className="blockquote">
-                        <p className="mb-0">"Sports is the greatest physical poetry"</p>
-                    </blockquote>
+                  <small className="title--event">
+                    7<sup>th</sup>
+                  </small>
+                  <h1 className="title--gces">GCES</h1>
+                  <h2 className="title--expo">SPORTS MEET</h2>
+                  <p className="title--year">2018</p>
+                  <blockquote className="blockquote">
+                    <p className="mb-0">
+                      "Sports is the greatest physical poetry"
+                    </p>
+                  </blockquote>
                 </div>
-                  <hr/>
-                <div style={{ color: '#fff' }}>
+                <hr />
+                <div style={{ color: "#fff" }}>
                   <p>
-                    <span style={{ width: '50px', display: 'inline-block' }}>Date</span>: &nbsp; 10<sup>th</sup> TO 14<sup>th</sup> December
+                    <span style={{ width: "50px", display: "inline-block" }}>
+                      Date
+                    </span>
+                    : &nbsp; 10<sup>th</sup> TO 14<sup>th</sup> December
                   </p>
                   <p>
-                    <span style={{ width: '50px', display: 'inline-block' }}>Time</span>: &nbsp; 7 AM to 5 PM
+                    <span style={{ width: "50px", display: "inline-block" }}>
+                      Time
+                    </span>
+                    : &nbsp; 7 AM to 5 PM
                   </p>
                   <p>
-                    <span style={{ width: '50px', display: 'inline-block' }}>Venue</span>: &nbsp; GCES
+                    <span style={{ width: "50px", display: "inline-block" }}>
+                      Venue
+                    </span>
+                    : &nbsp; GCES
                   </p>
                 </div>
 
                 <hr />
                 <div>
-                  <span style={{ color: '#f8f8f8', margin: '8px 0' }}>Have any queries or questions?</span> &nbsp;
-                  <a className="btn btn-sm btn-action" href="mailto:ananta@codse.com?subject=About+IT+Expo">Contact Us</a>
+                  <span style={{ color: "#f8f8f8", margin: "8px 0" }}>
+                    Have any queries or questions?
+                  </span>{" "}
+                  &nbsp;
+                  <a
+                    className="btn btn-sm btn-action"
+                    href="mailto:mailarjunadhikari@gmail.com?subject=About+GCES+SPORTSMEET"
+                  >
+                    Contact Us
+                  </a>
                 </div>
               </div>
             </div>
-            <div className="col-md-6" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-            {
-              isBusy && (
-                <div style={{ textAlign: 'center' }}>
-                  <i className="fa fa-circle-o-notch fa-spin" style={{ fontSize: '40px' }}></i>
+            <div
+              className="col-md-6"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center"
+              }}
+            >
+              {isBusy && (
+                <div style={{ textAlign: "center" }}>
+                  <i
+                    className="fa fa-circle-o-notch fa-spin"
+                    style={{ fontSize: "40px" }}
+                  ></i>
                 </div>
-              )
-            }
+              )}
               <div id="events" className="carousel slide" data-ride="carousel">
                 <div className="carousel-inner slider">
                   <ol className="carousel-indicators">
-                    {
-                      events.map((event, index) => <li key={index} data-target="#events" data-slide-to={index} className={index === 0 ? 'active' : ''}></li>)
-                    }
+                    {events.map((event, index) => (
+                      <li
+                        key={index}
+                        data-target="#events"
+                        data-slide-to={index}
+                        className={index === 0 ? "active" : ""}
+                      ></li>
+                    ))}
                   </ol>
-                  {
-                    events.map((event, index) => <Slide key={'event+' + index} active={index === 0} event={event} />)
-                  }
+                  {events.map((event, index) => (
+                    <Slide
+                      key={"event+" + index}
+                      active={index === 0}
+                      event={event}
+                    />
+                  ))}
                 </div>
               </div>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import Header from '../components/Header';
-import About from './About';
-import Landing from './Landing';
-import Download from './Download';
-import Tiesheet from './Tiesheet';
-import Timetable from './Timetable';
+import Header from "../components/Header";
+import About from "./About";
+import Landing from "./Landing";
+import Download from "./Download";
+import Tiesheet from "./Tiesheet";
+import Timetable from "./Timetable";
 
-export const TiesheetURL = "https://raw.githubusercontent.com/anantabastola/gces-sportsmeet/master/src/data/Tiesheet.json";
-export const TimetableURL = "https://raw.githubusercontent.com/anantabastola/gces-sportsmeet/master/src/data/TimeTable.json";
-export const DownloadURL = "https://raw.githubusercontent.com/anantabastola/gces-sportsmeet/master/src/data/Download.json";
-export const EventsURL = "https://raw.githubusercontent.com/anantabastola/gces-sportsmeet/master/src/data/Events.json";
+export const TiesheetURL =
+  "https://raw.githubusercontent.com/anantabastola/gces-sportsmeet/master/src/data/Tiesheet.json";
+export const TimetableURL =
+  "https://raw.githubusercontent.com/anantabastola/gces-sportsmeet/master/src/data/TimeTable.json";
+export const DownloadURL =
+  "https://raw.githubusercontent.com/anantabastola/gces-sportsmeet/master/src/data/Download.json";
+export const EventsURL =
+  "https://raw.githubusercontent.com/anantabastola/gces-sportsmeet/master/src/data/Events.json";
 class Home extends React.Component {
-
   render() {
     return (
-        <BrowserRouter>
-            <section>
-                <Header/>
-                <Route path="/about" component={About}/>
-                <Route path="/downloads" component={Download}/>
-                <Route path="/tiesheet" component={Tiesheet}/>
-                <Route path="/timetable" component={Timetable}/>
-                <Route exact path="/" component={Landing}/>
-                <footer>
-                    <p><em>Powered by: <a href="http://codse.com">Codse</a></em></p>
-                </footer>
-            </section>
-        </BrowserRouter>
-      );
-    }
+      <BrowserRouter>
+        <section>
+          <Header />
+          <Route path="/about" component={About} />
+          <Route path="/downloads" component={Download} />
+          <Route path="/tiesheet" component={Tiesheet} />
+          <Route path="/timetable" component={Timetable} />
+          <Route exact path="/" component={Landing} />
+          <footer>
+            <p>
+              <em>
+                Powered by:{" "}
+                <a href="http://itclub.gces.edu.np/">GCES IT CLUB</a>
+              </em>
+            </p>
+          </footer>
+        </section>
+      </BrowserRouter>
+    );
   }
+}
 
-  export default Home;
+export default Home;
